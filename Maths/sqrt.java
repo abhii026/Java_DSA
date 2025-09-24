d38dde0389ad9695cc427b6a9ccf37e45d027f77
Abhishek Singh <iabhii026@gmail.com>
2025-09-24 20:50:54 +0530

    Add sqrt.java

---
+++ Maths/sqrt.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class sqrt {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = input.nextInt();
+        int p = 3;
+        double res = sqrt(num, p);
+        System.out.printf("%.3f", res);
+    }
+
+    static double sqrt(int num, int p) {
+        int start = 0;
+        int end = num;
+        double root = 0.0;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid * mid == num) {
+                return mid;
+            } else if (mid * mid > num) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        double inc = 0.1;
+        for (int i = 0; i < p; i++) {
+            while (root * root <= num) {
+                root += inc;
+            }
+            root -= inc;
+            inc /= 10;
+        }
+        return root;
+    }
+}
